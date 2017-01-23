042d36094c67f99b426e87885d31330e07e90a4c
Ana Schwendler <schwendler@gmail.com>
2017-01-23 15:27:03 -0200

    Adds histories page

--- js/app.js
+++ js/app.js
@@ -24,11 +24,57 @@ var createThumb = function (post) {
 
 };
 
+var createHistThumb = function (post) {
+
+  var link = document.createElement('a');
+  link.setAttribute('href', post.url);
+
+  var row = document.createElement('div');
+  row.setAttribute('class', 'row');
+
+  var col = document.createElement('div');
+  col.setAttribute('class', 'col-md-4');
+
+  var thumb = document.createElement('div');
+  thumb.setAttribute('class', 'histories-thumb');
+
+  var img = document.createElement('img');
+  img.setAttribute('src', post.image);
+
+  var d_col = document.createElement('div');
+  d_col.setAttribute('class', 'col-md-8');
+
+  var d_thumb = document.createElement('div');
+  d_thumb.setAttribute('class', 'histories-description');
+
+  var title = document.createElement('h3');
+  title.innerHTML = post.title;
+
+  var text = document.createElement('p');
+  text.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec laoreet, augue et dignissim consequat, nibh risus pretium mi, at congue sem enim nec lectus. Ut fringilla lacus at nibh mattis finibus.';
+
+  d_thumb.appendChild(title);
+  d_thumb.appendChild(text);
+  d_col.appendChild(d_thumb);
+  thumb.appendChild(img);
+  col.appendChild(thumb);
+  row.appendChild(col);
+  row.appendChild(d_col);
+  link.appendChild(row);
+
+  return link;
+}
+
 var loadPosts = function (posts) {
   for (var i = 0; i < 3; i++) {
       var post = createThumb(posts[i]);
       $('.posts-thumb-area').append(post);
   }
+
+  posts.forEach(function(post) {
+    var post = createHistThumb(post);
+    $('.histories-thumb-area').append(post);
+  });
 };
 
 $(document).ready(function(){
