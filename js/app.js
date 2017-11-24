e9ca4a557ccb7a00648fd262fb5eede7ccc649f8
Thulio Philipe <thulioph@gmail.com>
2017-11-24 20:43:52 -0300

    Updates the app.js to work with one generic method and display the post data.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 // Classes
 // ====
 
-class Posts {
+class MediumPosts {
   constructor() {
     this.postsUrl = 'https://medium-dsbr-proxy.herokuapp.com/';
 
     this.buildPosts = this.buildPosts.bind(this);
+    this.displayPosts = this.displayPosts.bind(this);
+    this.createPostCard = this.createPostCard.bind(this);
     this.handleError = this.handleError.bind(this);
-    this.createHomeThumbs = this.createHomeThumbs.bind(this);
-    this.createStoriesThumbs = this.createStoriesThumbs.bind(this);
   }
 
   loadPosts() {
@@ -20,33 +20,32 @@ class Posts {
       .catch((err) => this.handleError(err));
   }
 
-  handleError(err) {
-    throw new Error(err);
-  }
-
   buildPosts(posts) {
     let homePosts = document.querySelectorAll('.posts-thumb-area');
-    
-    if (homePosts.length) {
-      homePosts.forEach((el) => el.innerHTML = '');
-
-      for (let i = 0; i < 3; i++) {
-        homePosts.forEach((el) => el.append(this.createHomeThumbs(posts[i])));
-      }
-    }
-
     let storiesPosts = document.querySelectorAll('.histories-thumb-area');
 
-    if (storiesPosts.length) {
-      storiesPosts.forEach((el) => el.innerHTML = '');
+    if (homePosts.length) {
+      this.displayPosts(homePosts, posts, 3);
+    }
 
-      posts.forEach((post) => {
-        storiesPosts.forEach((el) => el.append(this.createStoriesThumbs(post)));
-      });
+    if (storiesPosts.length) {
+      this.displayPosts(storiesPosts, posts);
     }
   }
 
-  createHomeThumbs(post) {
+  displayPosts(container, data, limit = data.length) {
+    container.forEach((element) => element.innerHTML = '');
+
+    data.slice(0, limit).forEach((post) => {
+      container.forEach((el) => {
+        el.append(
+          this.createPostCard(post)
+        )
+      })
+    })
+  }
+
+  createPostCard(post) {
     const { image, snippet, title, url } = post;
 
     let postCard = document.createElement('article');
@@ -54,10 +53,10 @@ class Posts {
 
     let postImage = document.createElement('figure');
     postImage.setAttribute('class', 'post-card_image');
-    
+
     let postImg = document.createElement('img');
     postImg.setAttribute('src', image);
-    
+
     let postTitle = document.createElement('h3');
     postTitle.setAttribute('class', 'post-card_title');
     postTitle.innerHTML = title;
@@ -79,53 +78,13 @@ class Posts {
     return postCard;
   }
 
-  createStoriesThumbs(post) {
-    // const { image, snippet, title, url } = post;
-
-    let link = document.createElement('a');
-    link.setAttribute('href', post.url);
-
-    let row = document.createElement('div');
-    row.setAttribute('class', 'row');
-
-    let col = document.createElement('div');
-    col.setAttribute('class', 'col-md-4');
-
-    let thumb = document.createElement('div');
-    thumb.setAttribute('class', 'histories-thumb');
-    thumb.setAttribute('style', 'background-image:  url(' + post.image + ')');
-
-    let d_col = document.createElement('div');
-    d_col.setAttribute('class', 'col-md-8');
-
-    let d_thumb = document.createElement('div');
-    d_thumb.setAttribute('class', 'histories-description');
-
-    let title = document.createElement('h3');
-    title.innerHTML = post.title;
-
-    let text = document.createElement('p');
-    text.innerHTML = post.snippet;
-
-    // append
-    d_thumb.appendChild(title);
-    d_thumb.appendChild(text);
-
-    d_col.appendChild(d_thumb);
-
-    col.appendChild(thumb);
-
-    row.appendChild(col);
-    row.appendChild(d_col);
-
-    link.appendChild(row);
-
-    return link;
+  handleError(err) {
+    throw new Error(err);
   }
 }
 
 // Initialize
 // ====
 
-const SerenataPosts = new Posts();
+const SerenataPosts = new MediumPosts();
 SerenataPosts.loadPosts();
